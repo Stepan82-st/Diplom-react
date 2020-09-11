e2b355d4370ec7011a76b2057c791efd784e5273
Yaroslav Bryl <YaroslavBryl@ibagroup.eu>
2020-09-11 22:11:45 +0300

    Fix header component

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import logo from '../logo.svg';
+
+const Header = () => {
+  return (<header className={'header'}>
+    <img src={'favicon.ico'} alt={logo}/>
+  </header>);
+}
+
+export default Header;
