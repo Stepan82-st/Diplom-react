245971f7425d7d91f77df322e86f8ea05151d42b
Stepan82-st <bryl.step@tut.by>
2020-09-11 21:47:09 +0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className={'app-wrapper'}>
-        <header className={'header'}>
-            <img src={'favicon.ico'} alt={logo}/>
-            <h1> Sadovay ferma </h1>
-        </header>
+        <Header />
+
         <nav className={'nav'}>
           <div className={'buttons'}><a>Выбрать товарищество</a></div>
           <div className={'buttons'}><a>Новости</a></div>
@@ -17,8 +16,12 @@ function App() {
           <div className={'buttons'}><a>Реклама</a></div>
         </nav>
         <div className={'sidebar'}>
+            <h1 className={'item'}> Sadovay ferma </h1>
          </div>
-         <div className={'footer'}><p>Наш девиз - "Умрем с лопатой в руках"</p></div>
+         <div className={'footer'}>
+             <p className={'item1'}>Наш девиз - "Умрем с лопатой в руках"</p>
+             <p className={'item2'}>г.Минск 2020 ИИТ БГУИР</p>
+         </div>
     </div>
 
   );
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import logo from './logo.svg';
+
+function App() {
+    return (<header className={'header'}>
+        <img src={'favicon.ico'} alt={logo}/>
+    </header>);
+}
+export default 'Header';
